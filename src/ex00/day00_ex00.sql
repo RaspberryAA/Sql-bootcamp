6fc928cd83952d3e34649a0dd164f461ca40c601
RaspberryAA <avokado02@gmail.com>
2025-04-07 17:35:51 +0300

    rmd old folder

--- src/ex00/day00_ex00.sql
+++
@@ -1,2 +0,0 @@
---  Please make a select statement which returns all person's names and person's ages from the city ‘Kazan’.
-SELECT name, age FROM person WHERE address = 'Kazan';
--- src/ex08/day00_ex08.sql
+++
@@ -1,6 +0,0 @@
--- Create an SQL statement that returns all columns from the person_order table with rows whose identifier is an even number. 
--- The result must be ordered by the returned identifier.
-
-select * from person_order
-where id % 2 = 0
-order by id;
