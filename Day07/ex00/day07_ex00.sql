a353a2bba64e10fef4c54aa83c8516549fd484a7
RaspberryAA <avokado02@gmail.com>
2025-04-07 18:20:04 +0300

    Day07 ex00 fix

--- Day07/ex00/day07_ex00.sql
+++ Day07/ex00/day07_ex00.sql
@@ -1,4 +1,4 @@
--- Let's make a simple aggregation, please write a SQL statement that returns person identifiers and 
+-- Our knowledge way is incremental and linear therefore please be aware all changes that you made in Day03 during Exercises 07-13 and in Day04 during Exercise 07 should be on place -- Let's make a simple aggregation, please write a SQL statement that returns person identifiers and 
 -- corresponding number of visits in any pizzerias and sorts by number of visits in descending mode and sorts by
 --  person_id in ascending mode. Please take a look at the sample of data below.
 -- 
