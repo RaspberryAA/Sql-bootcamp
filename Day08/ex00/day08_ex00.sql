6a96cfe9fd456df2dab653db3a952ed6c9609560
RaspberryAA <avokado02@gmail.com>
2025-04-07 18:36:58 +0300

    Day08

--- Day08/ex00/day08_ex00.sql
+++ Day08/ex00/day08_ex00.sql
@@ -1,8 +1,7 @@
 -- Our knowledge way is incremental and linear therefore please be aware all changes that you made in Day03 during Exercises 07-13 and in Day04 during Exercise 07 should be on place 
 -- 
 -- Files to turn-in	day08_ex00.sql with comments for Session #1, Session #2 statements; screenshot of psql output for Session #1; screenshot of psql output for Session #2
--- Allowed	
--- Language	SQL
+--
 -- Please use the command line for PostgreSQL database (psql) for this task. You need to check how your changes will be published to the database for other database users.
 -- 
 -- Actually, we need two active sessions (i.e. 2 parallel sessions in the command line).
@@ -40,7 +39,8 @@
 -- You can see that the same query returns different results because the first query was run before publishing in Session#1 and the second query was run after Session#1 was finished.
 -- 
 -- 
--- 
+
+ 
 SESSION #1
 begin; 
 update pizzeria set rating = 5 where name = 'Pizza Hut';
--- Day08/ex01/day08_ex01.sql
+++ Day08/ex01/day08_ex01.sql
@@ -3,12 +3,15 @@
 -- 
 -- Actually, we need two active sessions (i.e. 2 parallel sessions in the command line).
 -- 
--- Before running a task, make sure you are at a standard isolation level in your database. Just run the following statement SHOW TRANSACTION ISOLATION LEVEL; and the result should be "read committed".
+-- Before running a task, make sure you are at a standard isolation level in your database.
+--  Just run the following statement SHOW TRANSACTION ISOLATION LEVEL; and the result should be "read committed".
 -- 
 -- If not, please set the read committed isolation level explicitly on a session level.
 -- 
--- Let's examine one of the famous "Lost Update Anomaly" database patterns. You can see a graphical representation of this anomaly on a picture. The horizontal red line represents the final results after all the sequential steps for both Sessions.	D08_06
--- Please check a rating for "Pizza Hut" in a transaction mode for both sessions and then make an UPDATE of the rating to a value of 4 in Session #1 and make an UPDATE of the rating to a value of 3.6 in Session #2 (in the same order as in the picture).
+-- Let's examine one of the famous "Lost Update Anomaly" database patterns. 
+-- You can see a graphical representation of this anomaly on a picture. The horizontal red line represents the final results after all the sequential steps for both Sessions.
+-- Please check a rating for "Pizza Hut" in a transaction mode for both sessions and then make an UPDATE of the rating to a value of 4 in Session #1 
+-- and make an UPDATE of the rating to a value of 3.6 in Session #2 (in the same order as in the picture).
 
 -- SESSION #1
 begin; 
